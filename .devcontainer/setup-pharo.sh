eadabc2120cd8cefdd506269cfe03462842d7c95
ahobbsy <48050187+ahobbsy@users.noreply.github.com>
2026-08-28 12:52:27 +0100

    Create setup-pharo.sh

---
+++ .devcontainer/setup-pharo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "=== Downloading Pharo 64-bit Linux VM and Image ==="
+curl https://get.pharo.org/64/ | bash
+#wget -qO- https://get.pharo.org/64/150 | bash
+
+echo "=== Ensuring permissions are correct ==="
+chmod +x pharo
+chmod +x pharo-ui
+
+echo "=== Pharo setup complete! ==="
